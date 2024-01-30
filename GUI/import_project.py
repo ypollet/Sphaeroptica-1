Fix : Virtual Camera scaling up/down during resizedEvent

GUI/import_project.py
@@ -112,9 +112,11 @@ class _IntrinsicsWidget(QWidget):
         rows_cam_mat = int(b_cam_mat.find('rows').text)
         cols_cam_mat = int(b_cam_mat.find('cols').text)
         cam_mat = b_cam_mat.find('data').text
-
-        width = Bs_data.find('image_Width').text
-        height = Bs_data.find('image_Height').text
+        print(len(Bs_data.find('image_Width').text))
+        print(Bs_data.find('image_Width').text)
+        width = int(Bs_data.find('image_Width').text)
+        
+        height = int(Bs_data.find('image_Height').text)
 
         intrinsics = {}
 
@@ -122,14 +124,15 @@ class _IntrinsicsWidget(QWidget):
         intrinsics["height"] = height
 
         intrinsics["camera matrix"] = {}
-        rows_cam_mat = int(b_cam_mat.find('rows').text)
         intrinsics["camera matrix"]["shape"] = [rows_cam_mat, cols_cam_mat]
         intrinsics["camera matrix"]["matrix"] = np.matrix(cam_mat).reshape((cols_cam_mat, rows_cam_mat)).tolist()
 
         intrinsics["distortion matrix"] = {}
-        rows_dist_coeffs = int(b_dist_coeffs.find('rows').text)
         intrinsics["distortion matrix"]["shape"] = [rows_dist_coeffs, cols_dist_coeffs]
         intrinsics["distortion matrix"]["matrix"] = np.matrix(dist_coeffs).reshape((cols_dist_coeffs, rows_dist_coeffs)).tolist()
+
+        print(intrinsics["camera matrix"]["matrix"])
+        print(["Hello", "Bonjour"])
 
         return intrinsics
         

scripts/additional/import_cameras_from_metashape.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     ap.add_argument("-i", "--input", required=True,
                     help="path to input OPK File")
     ap.add_argument("-o", "--output", required=False, default=None,
-                    help="path to output CSV File")
+                    help="path to output JSON File")
     args = vars(ap.parse_args())
 
     input_path = Path(args["input"])
@@ -104,7 +104,7 @@ if __name__ == '__main__':
 
 
         #looks like it's zyx
-        mat = reconstruction.rotate_x_axis(math.radians(180))@ np.matrix([[r11, r12, r13],
+        mat = np.matrix([[r11, r12, r13],
                         [r21, r22, r23],
                         [r31, r32, r33]])
         mat_mul = np.matmul(Rx, np.matmul(Ry, Rz))

scripts/additional/show_extrinsics.py
@@ -64,31 +64,43 @@ if __name__ == '__main__':
 
     proj_points = []
     for image in extrinsics:
-        print(image)
         matrix = np.matrix(extrinsics[image]["matrix"])
         rotation = matrix[0:3,0:3]
         trans = matrix[0:3,3]
-
         C = converters.get_camera_world_coordinates(rotation, trans)
-        print(C)
-        ax.scatter(C.item(0), C.item(1), C.item(2), color="blue")
-
-        ray_x = (rotation[0]).T
-        ax.quiver(C.item(0), C.item(1), C.item(2), ray_x.item(0), ray_x.item(1), ray_x.item(2), length=0.03, color="red")
-        ray_y = (rotation[1]).T
-        ax.quiver(C.item(0), C.item(1), C.item(2), ray_y.item(0), ray_y.item(1), ray_y.item(2), length=0.03, color="blue")
-        ray_z = (rotation[2]).T
-        ax.quiver(C.item(0), C.item(1), C.item(2), ray_z.item(0), ray_z.item(1), ray_z.item(2), length=0.03, color="green")
-
+        if image.startswith('_x_00200_'):
+            if image == '_x_00200_y_00000_.jpg':
+                ax.scatter(C.item(0), C.item(1), C.item(2), color="green", label="frontal view")
+            else:
+                ax.scatter(C.item(0), C.item(1), C.item(2), color="red")
+            
+        else:
+            ax.scatter(C.item(0), C.item(1), C.item(2), color="blue")
+        
+        ray_x = (rotation[0])
+        ax.quiver(C.item(0), C.item(1), C.item(2), ray_x.item(0), ray_x.item(1), ray_x.item(2), length=0.05, color="red")
+        ray_y = (rotation[1])
+        ax.quiver(C.item(0), C.item(1), C.item(2), ray_y.item(0), ray_y.item(1), ray_y.item(2), length=0.05, color="blue")
+        ray_z = (rotation[2])
+        ax.quiver(C.item(0), C.item(1), C.item(2), ray_z.item(0), ray_z.item(1), ray_z.item(2), length=0.05, color="green")
+        if ray_x.dot(ray_y.T) > 1e-6 or ray_x.dot(ray_z.T) > 1e-6 or ray_z.dot(ray_y.T) > 1e-6 :
+            print(f"Angles not perpendicular for image {image}")
         center += C
     center = center/len(extrinsics)
     print(center)
     ax.scatter(center.item(0), center.item(1), center.item(2), color="black", label='center')
+    ray_x = np.array([1,0,0]).T
+    ax.quiver(center.item(0), center.item(1), center.item(2), ray_x.item(0), ray_x.item(1), ray_x.item(2), length=0.05, color="red")
+    ray_y = np.array([0,1,0]).T
+    ax.quiver(center.item(0), center.item(1), center.item(2), ray_y.item(0), ray_y.item(1), ray_y.item(2), length=0.05, color="blue")
+    ray_z = np.array([0,0,1]).T
+    ax.quiver(center.item(0), center.item(1), center.item(2), ray_z.item(0), ray_z.item(1), ray_z.item(2), length=0.05, color="green")
 
     '''
     ax.set_xlim3d(-0.3, 0.3)
     ax.set_ylim3d(-0.3, 0.3)
     ax.set_zlim3d(-0.22, 0.22)'''
+    plt.legend(loc="upper right")
     plt.show()
 
     calib_file.close()